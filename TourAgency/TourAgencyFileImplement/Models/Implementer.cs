e0ea747379d0eb02ad3e2807b1142d07d349f9a6
vladislavpetrov21 <21vlapet@gmail.com>
2020-06-23 20:04:23 +0400

    f

--- TourAgency/TourAgencyFileImplement/Models/Implementer.cs
+++ TourAgency/TourAgencyFileImplement/Models/Implementer.cs
@@ -10,6 +10,5 @@ namespace TourAgencyFileImplement.Models
         public string ImplementerFIO { get; set; }
         public int WorkingTime { get; set; }
         public int PauseTime { get; set; }
-        public List<Order> Orders { get; set; }
     }
 }
