7365cbfe0215033fb4120c9f981832986d97fdda
vladislavpetrov21 <21vlapet@gmail.com>
2020-06-23 14:17:14 +0400

    fix

--- TourAgency/TourAgencyBusinessLogic/ViewModels/MessageInfoViewModel.cs
+++ TourAgency/TourAgencyBusinessLogic/ViewModels/MessageInfoViewModel.cs
@@ -14,15 +14,19 @@ namespace TourAgencyBusinessLogic.ViewModels
         public string MessageId { get; set; }
         [Column(title: "Отправитель", width: 150)]
         [DataMember]
+        [DisplayName("Отправитель")]
         public string SenderName { get; set; }
         [Column(title: "Дата письма", width: 100)]
         [DataMember]
+        [DisplayName("Дата письма")]
         public DateTime DateDelivery { get; set; }
         [Column(title: "Заголовок", width: 150)]
         [DataMember]
+        [DisplayName("Заголовок")]
         public string Subject { get; set; }
         [Column(title: "Текст", gridViewAutoSize: GridViewAutoSize.Fill)]
         [DataMember]
+        [DisplayName("Текст")]
         public string Body { get; set; }
         public override List<string> Properties() => new List<string>
         {
--- TourAgency/TourAgencyBusinessLogic/ViewModels/OrderViewModel.cs
+++ TourAgency/TourAgencyBusinessLogic/ViewModels/OrderViewModel.cs
@@ -18,27 +18,35 @@ namespace TourAgencyBusinessLogic.ViewModels
         public int VoucherId { get; set; }
         [Column(title: "Клиент", width: 150)]
         [DataMember]
+        [DisplayName("Клиент")]
         public string ClientFIO { get; set; }
         [Column(title: "Исполнитель", width: 100)]
         [DataMember]
+        [DisplayName("Исполнитель")]
         public string ImplementerFIO { get; set; }
         [Column(title: "Путевка", width: 100)]
         [DataMember]
+        [DisplayName("Путевка")]
         public string VoucherName { get; set; }
         [Column(title: "Количество", width: 100)]
         [DataMember]
+        [DisplayName("Количество")]
         public int Count { get; set; }
         [Column(title: "Сумма", width: 50)]
         [DataMember]
+        [DisplayName("Сумма")]
         public decimal Sum { get; set; }
         [Column(title: "Статус", width: 100)]
         [DataMember]
+        [DisplayName("Статус")]
         public OrderStatus Status { get; set; }
         [Column(title: "Дата создания", width: 100)]
         [DataMember]
+        [DisplayName("Дата создания")]
         public DateTime DateCreate { get; set; }
         [Column(title: "Дата выполнения", width: 100)]
         [DataMember]
+        [DisplayName("Дата выполнения")]
         public DateTime? DateImplement { get; set; }
         public override List<string> Properties() => new List<string>
         {
